1363c741b4d837fbe80b1f1266293924e5f25e43
Aditya Dubey <149411436+meetadityadubey@users.noreply.github.com>
2024-03-16 18:58:38 +0530

    Add files via upload

---
+++ 01_basics/03-datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+let name = "aditya" //srting data type
+let age = 18 //number data type
+let isLoggedIn = true //boolean data type
+
+//types of data
+// number => 2 to power 53
+// bigint
+// srting => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// object
+
+// to know datatype of a value. it can be represented in two ways
+console.log(typeof "aditya");
+console.log(typeof age);
+console.log(typeof undefined); // undefined datatype
+console.log(typeof null); // object datatype
