491103d78275f95b56aee346c7cc4be606f704ef
Aditya Dubey <149411436+meetadityadubey@users.noreply.github.com>
2024-04-23 17:41:10 +0530

    Add files via upload

--- 05-iterations/while-loop.js
+++ 05-iterations/while-loop.js
@@ -1,6 +1,29 @@
-// let test = 0
-// while (test <= 10) {
-//     console.log(`Value = ${test}`);
-//     test = test + 1
-// }
+let test = 0
+while (test <= 10) {
+    console.log(`Value is ${test}`);
+    test++
+}
 
+let myArray = ['shaktiman', 'aditya', 'dubey']
+
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+let Score = 1
+
+do {
+    console.log(`Value is ${Score}`)
+    Score++
+} while (Score <= 10);
+
+
+let Score1 = 11
+
+do {
+    console.log(`Value is ${Score1}`)
+    Score1++
+} while (Score1 <= 10);
